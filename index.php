98b0751f6ffad17d8b92cc17a1206e008ca5f525
Driss <driss@hotmail.com>
2023-02-01 23:23:10 +0100

    navbard +image

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Page avec barre de navigation</title>
+</head>
+<body>
+<header>
+    <nav>
+        <ul>
+            <li><a href="#accueil">Accueil</a></li>
+            <li><a href="#aide_devoir">Aide au devoir</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <img src="newyork.jpg" alt="Image de New York">
+</main>
+<footer>
+    <p>Contact: contact@example.com</p>
+    <p>Adresse: 123 Main St, New York, NY 10001</p>
+</footer>
+</body>
+</html>
+
+
